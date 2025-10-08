454deb1533df755b6c9e894bf6eaa71a61b5eac7
Dinesh_Mali <159063788+dinesh10123@users.noreply.github.com>
2025-10-08 17:50:46 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0){
+            return false;
+        }
+        int rev = 0;
+        int num = x;
+        while(x != 0){
+            int ld = x % 10;
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && ld > 7))
+                return false;
+            rev = (rev * 10) + ld;
+            x = x / 10;
+        }
+        return num == rev;
+    }
+}
